c1837c983c2a3cf79c88a9b067e39f36a0f78562
Toropov Yaroslav <ytoropov@saber3d.by>
2024-10-21 15:56:48 +0300

    Improve roslyn configuration

--- lua/kickstart/plugins/roslyn.lua
+++ lua/kickstart/plugins/roslyn.lua
@@ -6,11 +6,15 @@ return {
   ft = 'cs',
   config = function()
     require('roslyn').setup {
-      broad_search = true,
+      -- broad_search = true,
+      filewatch = true,
       ---@diagnostic disable-next-line: missing-fields
       config = {
         settings = {
-          ['csharp|inlay_hints'] = {
+          ["csharp|background_analysis"] = {
+            dotnet_compiler_diagnostics_scope = "fullSolution"
+          },
+          ["csharp|inlay_hints"] = {
             csharp_enable_inlay_hints_for_implicit_object_creation = true,
             csharp_enable_inlay_hints_for_implicit_variable_types = true,
             csharp_enable_inlay_hints_for_lambda_parameter_types = true,
@@ -24,14 +28,9 @@ return {
             dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
             dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
           },
-          ['csharp|code_lens'] = {
+          ["csharp|code_lens"] = {
             dotnet_enable_references_code_lens = true,
-          },
-          ['csharp|background_analysis'] = {
-            dotnet_analyzer_diagnostics_scope = true,
-            dotnet_compiler_diagnostics_scope = true,
-          },
-        },
+          },        },
       },
     }
   end,
