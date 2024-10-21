2d0420a137cf966678f4f7b8376d2f072208849f
Toropov Yaroslav <ytoropov@saber3d.by>
2024-10-21 10:09:45 +0300

    Moved roslyn plugin to separate file. Change shiftwidth.

--- init.lua
+++ init.lua
@@ -66,7 +66,7 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -183,7 +183,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -231,7 +231,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -327,7 +327,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
 
       -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
@@ -342,7 +342,7 @@ require('lazy').setup({
         },
       },
       { 'Bilal2453/luvit-meta', lazy = true },
-      { 'b0o/schemastore.nvim' }
+      { 'b0o/schemastore.nvim' },
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -497,9 +497,9 @@ require('lazy').setup({
           settings = {
             json = {
               schemas = require('schemastore').json.schemas(),
-              validate = { enable = true }
-            }
-          }
+              validate = { enable = true },
+            },
+          },
         },
         lua_ls = {
           -- cmd = {...},
@@ -547,13 +547,7 @@ require('lazy').setup({
       }
     end,
   },
-  {
-    'seblj/roslyn.nvim',
-    ft = "cs",
-    config = function()
-      require('roslyn').setup {}
-    end
-  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -703,7 +697,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
-          { name = 'buffer' }
+          { name = 'buffer' },
         },
       }
     end,
@@ -714,14 +708,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
+      vim.cmd.colorscheme 'gruvbox'
+      -- vim.o.background = dark
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
