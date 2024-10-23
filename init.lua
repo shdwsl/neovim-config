fda66e575455129c6a45818d1d155a2b41f94aa1
shdwsl <toryar13@gmail.com>
2024-10-23 17:20:47 +0300

    Replace onedark plugin

--- init.lua
+++ init.lua
@@ -770,13 +770,17 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'ellisonleao/gruvbox.nvim',
-    'olimorris/onedarkpro.nvim',
+    'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'gruvbox'
+      require('onedark').setup {
+        style = 'warmer'
+      }
+      require('onedark').load();
       vim.cmd.colorscheme = 'onedark'
       -- vim.o.background = dark
       -- You can configure highlights by doing something like:
