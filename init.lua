8c098b8c19921905100f22d0303fe96684a3575d
Toropov Yaroslav <ytoropov@saber.games>
2025-04-09 19:23:30 +0300

    replaced with catpuccion theme

--- init.lua
+++ init.lua
@@ -617,14 +617,15 @@ else
         -- change the command in the config to whatever the name of that colorscheme is.
         --
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'ellisonleao/gruvbox.nvim',
+        -- 'ellisonleao/gruvbox.nvim',
+        'catppuccin/nvim',
         -- 'navarasu/onedark.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
         init = function()
           -- Load the colorscheme here.
           -- Like many other themes, this one has different styles, and you could load
           -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-          vim.cmd.colorscheme 'gruvbox'
+          vim.cmd.colorscheme 'catppuccin'
           -- require('onedark').setup {
           --   style = 'warmer',
           -- }
