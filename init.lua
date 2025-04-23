0430cbd70e189ad8115fddbf5a04b0c5087d3c56
shdwsl <toryar13@gmail.com>
2025-04-23 09:32:21 +0300

    set concrete color palette in theme

--- init.lua
+++ init.lua
@@ -630,7 +630,7 @@ else
           -- Load the colorscheme here.
           -- Like many other themes, this one has different styles, and you could load
           -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-          vim.cmd.colorscheme 'catppuccin'
+          vim.cmd.colorscheme 'catppuccin-mocha'
           -- require('onedark').setup {
           --   style = 'warmer',
           -- }
