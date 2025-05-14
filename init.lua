b6bdbe993eef0db47d2f2b724bb5fe8b793ce08f
shdwsl <toryar13@gmail.com>
2025-05-14 22:06:59 +0300

    disabled roslyn package

--- init.lua
+++ init.lua
@@ -740,7 +740,7 @@ else
       -- require 'kickstart.plugins.lint',
       require 'kickstart.plugins.autopairs',
       require 'kickstart.plugins.neo-tree',
-      require 'kickstart.plugins.roslyn',
+      -- require 'kickstart.plugins.roslyn',
       require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
       require 'kickstart.plugins.lazygit',
       -- require 'kickstart.plugins.neorg',
